fa03b5cc974f1108801ac4be4ebf6b654eeac102
Steve Ognibene <steve.ognibene@outlook.com>
2014-05-27 22:45:01 -0400

    start of improved tests

--- Code/TSqlFlex.Core.Tests/FlexResultSetTests.cs
+++ Code/TSqlFlex.Core.Tests/FlexResultSetTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,12 @@ namespace TSqlFlex.Core.Tests
             Assert.IsNotNull(fsr.exceptions);
             Assert.AreEqual(0, fsr.exceptions.Count);
 
+        }
+
+        public static DataTable FakeSchemaDataTable() {
+            var dt = new DataTable("test");
+            dt.LoadDataRow(new object[] { "Name", (int)0, (int)100, (short)255, (short)255, false, false, System.DBNull.Value, DBNull.Value, "Name", DBNull.Value, "MyStuff", typeof(String), false, (int)12, false, false, false, false, false, false, false, false, typeof(System.Data.SqlTypes.SqlString), "nvarchar", DBNull.Value, DBNull.Value, DBNull.Value, DBNull.Value, (int)12, false }, false);
+            return dt;
         }
     }
 }
