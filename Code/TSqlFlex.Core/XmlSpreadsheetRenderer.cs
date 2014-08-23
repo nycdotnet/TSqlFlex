783446f1ca7db808e6c3a77b054692392f5d277e
Steve Ognibene <steve.ognibene@outlook.com>
2014-08-22 22:51:56 -0400

    will no longer try to script a worksheet if the schema is null

--- Code/TSqlFlex.Core/XmlSpreadsheetRenderer.cs
+++ Code/TSqlFlex.Core/XmlSpreadsheetRenderer.cs
@@ -15,56 +15,59 @@ namespace TSqlFlex.Core
             for (int i = 0; i < resultSet.results.Count; i++)
             {
                 FlexResult result = resultSet.results[i];
-                int columnCount = result.visibleColumnCount;
-
-                sw.Write(String.Format("<Worksheet ss:Name=\"Sheet{0}\">", i + 1));
-                sw.Write(String.Format("<Table ss:ExpandedColumnCount=\"{0}\" ss:ExpandedRowCount=\"{1}\" x:FullColumns=\"1\" x:FullRows=\"1\" ss:DefaultRowHeight=\"15\">",
-                    columnCount,
-                    result.data.Count + 1 /* include header row */)
-                    );
-
-                //do header
-                sw.Write("<Row>");
-                for (int colIndex = 0; colIndex < columnCount; colIndex += 1)
+                if (result.schema != null)
                 {
-                    sw.Write(String.Format("<Cell ss:StyleID=\"s62\"><Data ss:Type=\"String\">{0}</Data></Cell>", escapeForXML((string)result.schema.Rows[colIndex].ItemArray[(int)FieldScripting.FieldInfo.Name])));
-                }
-                sw.Write("</Row>");
+                    int columnCount = result.visibleColumnCount;
 
-                //do data rows
-                for (int rowIndex = 0; rowIndex < result.data.Count; rowIndex += 1)
-                {
+                    sw.Write(String.Format("<Worksheet ss:Name=\"Sheet{0}\">", i + 1));
+                    sw.Write(String.Format("<Table ss:ExpandedColumnCount=\"{0}\" ss:ExpandedRowCount=\"{1}\" x:FullColumns=\"1\" x:FullRows=\"1\" ss:DefaultRowHeight=\"15\">",
+                        columnCount,
+                        result.data.Count + 1 /* include header row */)
+                        );
+
+                    //do header
                     sw.Write("<Row>");
                     for (int colIndex = 0; colIndex < columnCount; colIndex += 1)
                     {
-                        object fieldData = result.data[rowIndex][colIndex];
-                        string fieldTypeName = result.schema.Rows[colIndex].ItemArray[(int)FieldScripting.FieldInfo.DataType].ToString();
-                        if (fieldData == null || fieldData is DBNull)
-                        {
-                            sw.Write("<Cell/>");
-                        }
-                        else if (fieldTypeName == "bigint" || fieldTypeName == "numeric" || fieldTypeName == "smallint" || fieldTypeName == "decimal" || fieldTypeName == "smallmoney" ||
-                            fieldTypeName == "int" || fieldTypeName == "tinyint" || fieldTypeName == "float" || fieldTypeName == "real" || fieldTypeName == "money")
-                        {
-                            sw.Write(String.Format("<Cell><Data ss:Type=\"Number\">{0}</Data></Cell>\r\n", escapeForXML(fieldData.ToString())));
-                        }
-                        else if (fieldTypeName == "date" || fieldTypeName == "datetime2" || fieldTypeName == "time" || fieldTypeName == "datetime" ||
-                            fieldTypeName == "smalldatetime")
-                        {
-                            sw.Write(String.Format("<Cell ss:StyleID=\"s63\"><Data ss:Type=\"DateTime\">{0}</Data></Cell>\r\n", escapeForXML(
-                                ((DateTime)fieldData).ToString("yyyy-MM-ddTHH:mm:ss.fff")
-                                )));
-                        }
-                        else
-                        {
-                            sw.Write(String.Format("<Cell ss:StyleID=\"s64\"><Data ss:Type=\"String\">{0}</Data></Cell>\r\n", escapeForXML(result.data[rowIndex][colIndex].ToString())));
-                        }
-
+                        sw.Write(String.Format("<Cell ss:StyleID=\"s62\"><Data ss:Type=\"String\">{0}</Data></Cell>", escapeForXML((string)result.schema.Rows[colIndex].ItemArray[(int)FieldScripting.FieldInfo.Name])));
                     }
                     sw.Write("</Row>");
-                }
 
-                sw.Write("</Table></Worksheet>");
+                    //do data rows
+                    for (int rowIndex = 0; rowIndex < result.data.Count; rowIndex += 1)
+                    {
+                        sw.Write("<Row>");
+                        for (int colIndex = 0; colIndex < columnCount; colIndex += 1)
+                        {
+                            object fieldData = result.data[rowIndex][colIndex];
+                            string fieldTypeName = result.schema.Rows[colIndex].ItemArray[(int)FieldScripting.FieldInfo.DataType].ToString();
+                            if (fieldData == null || fieldData is DBNull)
+                            {
+                                sw.Write("<Cell/>");
+                            }
+                            else if (fieldTypeName == "bigint" || fieldTypeName == "numeric" || fieldTypeName == "smallint" || fieldTypeName == "decimal" || fieldTypeName == "smallmoney" ||
+                                fieldTypeName == "int" || fieldTypeName == "tinyint" || fieldTypeName == "float" || fieldTypeName == "real" || fieldTypeName == "money")
+                            {
+                                sw.Write(String.Format("<Cell><Data ss:Type=\"Number\">{0}</Data></Cell>\r\n", escapeForXML(fieldData.ToString())));
+                            }
+                            else if (fieldTypeName == "date" || fieldTypeName == "datetime2" || fieldTypeName == "time" || fieldTypeName == "datetime" ||
+                                fieldTypeName == "smalldatetime")
+                            {
+                                sw.Write(String.Format("<Cell ss:StyleID=\"s63\"><Data ss:Type=\"DateTime\">{0}</Data></Cell>\r\n", escapeForXML(
+                                    ((DateTime)fieldData).ToString("yyyy-MM-ddTHH:mm:ss.fff")
+                                    )));
+                            }
+                            else
+                            {
+                                sw.Write(String.Format("<Cell ss:StyleID=\"s64\"><Data ss:Type=\"String\">{0}</Data></Cell>\r\n", escapeForXML(result.data[rowIndex][colIndex].ToString())));
+                            }
+
+                        }
+                        sw.Write("</Row>");
+                    }
+
+                    sw.Write("</Table></Worksheet>");
+                }
             }
             sw.Write("</Workbook>\r\n");
         }
