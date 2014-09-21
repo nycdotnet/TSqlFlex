846462918bf8bc18ef0a48823a64d9587c779259
Steve Ognibene <steve.ognibene@outlook.com>
2014-09-21 19:57:47 -0400

    Initial UI for options dialog

---
+++ Code/TSqlFlex/OptionsControl.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace TSqlFlex
+{
+    partial class OptionsControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbScriptCompatibility = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkScriptIdentity = new System.Windows.Forms.CheckBox();
+            this.chkScriptTimestamp = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnResetDefaults = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBatchSeparator = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbResultNaming = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Script Compatibility";
+            // 
+            // cmbScriptCompatibility
+            // 
+            this.cmbScriptCompatibility.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbScriptCompatibility.FormattingEnabled = true;
+            this.cmbScriptCompatibility.Location = new System.Drawing.Point(299, 75);
+            this.cmbScriptCompatibility.Name = "cmbScriptCompatibility";
+            this.cmbScriptCompatibility.Size = new System.Drawing.Size(74, 21);
+            this.cmbScriptCompatibility.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Script TIMESTAMP";
+            // 
+            // chkScriptIdentity
+            // 
+            this.chkScriptIdentity.AutoSize = true;
+            this.chkScriptIdentity.Location = new System.Drawing.Point(122, 30);
+            this.chkScriptIdentity.Name = "chkScriptIdentity";
+            this.chkScriptIdentity.Size = new System.Drawing.Size(15, 14);
+            this.chkScriptIdentity.TabIndex = 3;
+            this.chkScriptIdentity.UseVisualStyleBackColor = true;
+            // 
+            // chkScriptTimestamp
+            // 
+            this.chkScriptTimestamp.AutoSize = true;
+            this.chkScriptTimestamp.Location = new System.Drawing.Point(122, 54);
+            this.chkScriptTimestamp.Name = "chkScriptTimestamp";
+            this.chkScriptTimestamp.Size = new System.Drawing.Size(15, 14);
+            this.chkScriptTimestamp.TabIndex = 5;
+            this.chkScriptTimestamp.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Script IDENTITY";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(122, 78);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Script Computed";
+            // 
+            // btnResetDefaults
+            // 
+            this.btnResetDefaults.Location = new System.Drawing.Point(472, 3);
+            this.btnResetDefaults.Name = "btnResetDefaults";
+            this.btnResetDefaults.Size = new System.Drawing.Size(100, 32);
+            this.btnResetDefaults.TabIndex = 8;
+            this.btnResetDefaults.Text = "Reset Defaults";
+            this.btnResetDefaults.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(299, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(112, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(198, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Execution Mode";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(198, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Batch Separator";
+            // 
+            // txtBatchSeparator
+            // 
+            this.txtBatchSeparator.Location = new System.Drawing.Point(299, 27);
+            this.txtBatchSeparator.Name = "txtBatchSeparator";
+            this.txtBatchSeparator.Size = new System.Drawing.Size(112, 20);
+            this.txtBatchSeparator.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(198, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Result Style";
+            // 
+            // cmbResultNaming
+            // 
+            this.cmbResultNaming.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbResultNaming.FormattingEnabled = true;
+            this.cmbResultNaming.Location = new System.Drawing.Point(299, 51);
+            this.cmbResultNaming.Name = "cmbResultNaming";
+            this.cmbResultNaming.Size = new System.Drawing.Size(112, 21);
+            this.cmbResultNaming.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 6);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Script Column Names";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(122, 6);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // OptionsControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.cmbResultNaming);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtBatchSeparator);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnResetDefaults);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chkScriptTimestamp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chkScriptIdentity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbScriptCompatibility);
+            this.Controls.Add(this.label1);
+            this.Name = "OptionsControl";
+            this.Size = new System.Drawing.Size(575, 104);
+            this.Load += new System.EventHandler(this.OptionsControl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbScriptCompatibility;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkScriptIdentity;
+        private System.Windows.Forms.CheckBox chkScriptTimestamp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnResetDefaults;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBatchSeparator;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbResultNaming;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
---
+++ Code/TSqlFlex/OptionsControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TSqlFlex
+{
+    public partial class OptionsControl : UserControl
+    {
+        public OptionsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OptionsControl_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
