4bda8642bb25f4fb44aeb887508682d1f6924d4c
Steve Ognibene <steve.ognibene@outlook.com>
2015-01-24 22:31:57 -0500

    Working Chocolatey Installer and Uninstaller

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -1,12 +1,55 @@
-$name = 'T-SQL Flex'
-$url  = 'https://github.com/nycdotnet/TSqlFlex/releases/download/0.0.10-alpha/TSqlFlex-v0.0.10-alpha.zip'
-$flexInstallPath = "$env:ProgramData\T-SQL Flex"
-
+﻿$packageName = "T-SQL Flex";
+$url = "https://github.com/nycdotnet/TSqlFlex/releases/download/0.0.10-alpha/TSqlFlex-v0.0.10-alpha.zip";
+$installLocation = "$env:ProgramData\T-SQL Flex";
 
 try {
-  Install-ChocolateyZipPackage $name $url $flexInstallPath
-  Write-ChocolateySuccess $name
+  Install-ChocolateyZipPackage "$packageName" "$url" "$installLocation"
 } catch {
-  Write-ChocolateyFailure $name
+	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message);"
+	throw;
 }
 
+function Create-RegistryKeyIfNotExists($parentKey, $testKey) {
+  if ((test-path "$parentKey\$testKey") -eq $false) {
+    New-Item -Path "$parentKey" -Name "$testKey" | Out-Null
+  }
+  if ((test-path "$parentKey\$testKey") -eq $true) {
+	return "$parentKey\$testKey";
+  }
+  throw "unable to create or confirm existence of '$parentKey\$testKey'";
+}
+
+# thanks: http://stackoverflow.com/questions/5648931/test-if-registry-value-exists
+function Test-RegistryValue($regkey, $name) {
+    $exists = Get-ItemProperty -Path "$regkey" -Name "$name" -ErrorAction SilentlyContinue
+    If (($exists -ne $null) -and ($exists.Length -ne 0)) {
+        Return $true
+    }
+    Return $false
+}
+
+function Get-RegistrySoftwareRootKey() {
+    if (Get-ProcessorBits -eq 64)  {
+	    Write-Debug "64-bit processor detected.";
+        Return "HKLM:\Software\Wow6432Node";
+    }
+	Write-Debug "32-bit processor detected.";
+	Return "HKLM:\Software";
+}
+
+try {
+	$root = Get-RegistrySoftwareRootKey;
+	$createdKey = $root;
+    $addKeys = "Red Gate\SIPFramework\Plugins";
+	$addKeys.Split("\") | % { $createdKey = Create-RegistryKeyIfNotExists $createdKey $_ }
+	
+	if ((Test-RegistryValue $createdKey "TSQLFlex") -eq $false) {
+		New-ItemProperty -Name "TSQLFlex" -Path $createdKey -Value "$installLocation\TSqlFlex.dll" | Out-Null
+	}
+
+} catch {
+	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message);"
+	throw;
+}
+
+Write-ChocolateySuccess "$packageName"
---
+++ Chocolatey/tools/chocolateyUninstall.ps1
@@ -0,0 +1,51 @@
+﻿$packageName = "T-SQL Flex";
+$installLocation = "$env:ProgramData\T-SQL Flex";
+
+function Remove-ItemIfExists($itemName) {
+	if ((Test-Path $itemName) -eq $true) {
+	    Remove-Item $itemName;
+	}
+}
+
+# thanks: http://stackoverflow.com/questions/5648931/test-if-registry-value-exists
+function Test-RegistryValue($regkey, $name) {
+    $exists = Get-ItemProperty -Path "$regkey" -Name "$name" -ErrorAction SilentlyContinue
+    If (($exists -ne $null) -and ($exists.Length -ne 0)) {
+        Return $true
+    }
+    Return $false
+}
+
+function Get-RegistrySoftwareRootKey() {
+    if (Get-ProcessorBits -eq 64)  {
+        Return "HKLM:\Software\Wow6432Node";
+    }
+	Return "HKLM:\Software";
+}
+
+try {
+	Remove-ItemIfExists "$installLocation\RedGate.SIPFrameworkShared.dll";
+	Remove-ItemIfExists "$installLocation\TSqlFlex.Core.dll";
+	Remove-ItemIfExists "$installLocation\TSqlFlex.dll";
+	Remove-ItemIfExists "$installLocation\InstallationInstructions.txt";
+	Remove-ItemIfExists "$installLocation\License.txt";
+	Remove-ItemIfExists "$installLocation\README.txt";
+	
+	$root = Get-RegistrySoftwareRootKey;
+	$pluginsPath = "$root\Red Gate\SIPFramework\Plugins";
+	if ((Test-RegistryValue $pluginsPath "TSQLFlex") -eq $true) {
+		Remove-ItemProperty -Name "TSQLFlex" -Path $pluginsPath | Out-Null
+	}
+	
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message);
+	throw;
+}
+
+try {
+  Remove-ItemIfExists "$installLocation";
+} catch {
+  # Don't sweat it.
+}
+
+Write-ChocolateySuccess "$packageName uninstall complete.";
